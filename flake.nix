15237129ae8770e1793fa6161c0cdf983abfd946
Robin Appelman <robin@icewind.nl>
2023-07-20 21:48:08 +0200

    basic nix setup

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [ (import rust-overlay) ];
+      pkgs = (import nixpkgs) {
+        inherit system overlays;
+      };
+      lib = pkgs.lib;
+      naersk' = pkgs.callPackage naersk {};
+      src = lib.sources.sourceByRegex (lib.cleanSource ./.) ["Cargo.*" "(src|derive|benches|tests|examples|crypto|protobuf)(/.*)?"];
+      nearskOpt = {
+        pname = "steam-vent";
+        root = src;
+      };
+    in rec {
+      packages = {
+        check = naersk'.buildPackage (nearskOpt // {
+          mode = "check";
+        });
+        clippy = naersk'.buildPackage (nearskOpt // {
+          mode = "clippy";
+        });
+        test = naersk'.buildPackage (nearskOpt // {
+          release = false;
+          mode = "test";
+        });
+      };
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy cargo-audit cargo-msrv cargo-fuzz];
+      };
+    });
+}
