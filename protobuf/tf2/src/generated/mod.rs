8fd8726c979f8954005cd37845a815547dbf690a
Robin Appelman <robin@icewind.nl>
2024-08-17 23:54:52 +0200

    add tf2 protobuffs

---
+++ protobuf/tf2/src/generated/mod.rs
@@ -0,0 +1,17 @@
+// @generated
+
+pub mod base_gcmessages;
+pub mod econ_gcmessages;
+pub mod gcsdk_gcmessages;
+pub mod gcsystemmsgs;
+pub mod steamdatagram_auth_messages;
+pub mod steamdatagram_messages;
+pub mod steamdatagram_messages_auth;
+pub mod steamdatagram_messages_sdr;
+pub mod steammessages;
+pub mod steammessages_gamenetworkingui;
+pub mod steamnetworkingsockets_messages;
+pub mod steamnetworkingsockets_messages_certs;
+pub mod steamnetworkingsockets_messages_udp;
+pub mod tf_gcmessages;
+pub mod tf_proto_def_messages;
