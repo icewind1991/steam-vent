30466d89a8e226b2ca4a22b1c42e7e89c1bd3b91
Roman <59799956+AspectUnk@users.noreply.github.com>
2024-08-17 11:31:18 +0300

    fix for job id & receive msg

---
+++ examples/product_info.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use steam_vent::{Connection, ServerList};
+use steam_vent_proto::steammessages_clientserver_appinfo::{cmsg_client_picsproduct_info_request, CMsgClientPICSProductInfoRequest, CMsgClientPICSProductInfoResponse};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let server_list = ServerList::discover().await?;
+    let connection = Connection::anonymous(server_list).await?;
+
+    let msg = CMsgClientPICSProductInfoRequest {
+        apps: vec![cmsg_client_picsproduct_info_request::AppInfo {
+            appid: Some(440),
+            only_public_obsolete: Some(true),
+            ..Default::default()
+        }],
+        meta_data_only: Some(true),
+        single_response: Some(true),
+        ..Default::default()
+    };
+
+    connection.send(msg).await?;
+    println!(
+        "response {:?}",
+        connection.one::<CMsgClientPICSProductInfoResponse>().await
+    );
+
+    Ok(())
+}
